718ee500773b720b772bd76ade438ff541c7c944
John Luo <donotletjohnretire@gmail.com>
2013-03-04 14:17:33 +0800

    [feature] add ViewFliper.java [description] it's a viewgroup, as a container of the fliper view, contains a top view and backgroud view.

---
+++ src/com/myriad/test/ViewFliper.java
@@ -0,0 +1,19 @@
+package com.myriad.test;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class ViewFliper extends ViewGroup {
+
+	public ViewFliper(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
